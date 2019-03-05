8a20f52cef017f897c9c35ab76784b30e0dec0d6
shraddha Kumbhar <48197434+shraddhakumbhar18@users.noreply.github.com>
2019-03-05 15:45:08 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     }
 
      public static void division(int number1,int number2) {
-        System.out.println("Division=" +(number1+number2));
+        System.out.println("Division=" +(number1/number2));
 
      public static void Substraction(int number1,int number2) {
         System.out.println("Substraction=" +(number1-number2));
